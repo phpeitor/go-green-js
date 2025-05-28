c4f5e72a3faac6a4cb4a42251a3e3f2d28fbfbac
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-05-28 00:02:04 -0500

    console table commit

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import random from "random";
 
 const path = "./data.json";
 
-const startDate = moment("2018-10-01");
-const endDate = moment("2018-12-30");
+const startDate = moment("2019-02-01");
+const endDate = moment("2019-07-30");
 const daysDiff = endDate.diff(startDate, "days");
+const datesTable = [];
 
 const makeCommits = (n) => {
-  if(n === 0) return simpleGit().push();
+  if (n === 0) {
+    console.table(datesTable);
+    return simpleGit().push();
+  }
   
   const randomDays = random.int(0, daysDiff);
   const date = startDate.clone().add(randomDays, "d").format();
@@ -20,11 +24,14 @@ const makeCommits = (n) => {
   };
   
   console.log(date);
+  datesTable.push({ "Commit": 100 - n + 1, "Fecha": date });
+
   jsonfile.writeFile(path, data, () => {
     simpleGit()
       .add([path])
       .commit(date, { "--date": date }, makeCommits.bind(this, --n));
   });
+  
 };
 
 makeCommits(100);
